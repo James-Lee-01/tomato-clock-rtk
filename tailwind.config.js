2f8b54bbf256718848abb017054787c1c7c870fa
James-Lee-01 <freebike01@gmail.com>
2024-01-29 01:28:07 +0800

    file: package daisyui setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+      themes: [
+        {
+          mytheme: {
+                      
+            "primary": "#fbbf24",
+                      
+            "secondary": "#2563eb",
+                      
+            "accent": "#854d0e",
+                      
+            "neutral": "#6b7280",
+                      
+            "base-100": "#1E293B",
+                      
+            "info": "#009cff",
+                      
+            "success": "#509b00",
+                      
+            "warning": "#f97316",
+                      
+            "error": "#e11d48",
+          },
+        },
+      ],
+    },
+  plugins: [daisyui],
 }
 
